ed72dc2c661553fe981a0d9c5a1f84b8867353d9
Nicholas D. Roman <NicholasDRoman@hotmail.com>
2019-05-08 08:48:32 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+//import "./App.css";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <Contact
+          name="Nicholas Roman"
+          email="Nick@gmail.com"
+          phone="888-999-9999"
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
