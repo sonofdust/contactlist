ca82f792cc972fc36918cf36ae57bf66cc8badfc
Nicholas D. Roman <NicholasDRoman@hotmail.com>
2019-05-09 11:23:22 -0600

    Added the ability to show detailed contact information

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,21 +2,30 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = { showContactInfo: true };
   onShowClick = () => {
-    console.log("Hello World");
+    this.setState({ showContactInfo: !this.state.showContactInfo });
   };
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
         <h4>
           {name}
-          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+          <i
+            onClick={this.onShowClick}
+            className={
+              this.state.showContactInfo ? "fas fa-sort-down" : "fas fa-sort-up"
+            }
+          />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
